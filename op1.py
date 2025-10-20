f857cb4d558e37f4d81271cc9e2b8f5f56bffc9d
Kauã Silva Valadares <ksvg77828@gmail.com>
2025-10-20 20:44:16 -0300

    correção

--- op1.py
+++ op1.py
@@ -1,4 +1,8 @@
+############################
 #Operação entre setas 
+############################
+print ("Operação entre setas ")
+print("------------------------")
 a = {1,2,3} 
 b = {3,4,5}
 
@@ -8,4 +12,47 @@ print ("uniao de a com b :",a|b )
 print ("interseçãqo :", a & b )
 #
 print ("diferença", a-b)
+
+#########################################
+ # operadores 
+ #  x == y, x!=y , x>y ,x<y, x>=y, x<=y
+#########################################
+
+print ("operadores")
+print("------------------------")
+
+x,y = 10,5
+print ("x=",x ,"y=",y )
+print ("x igualdade y", x == y)  
+print ("x diferente y " ,x!=y)  
+print ("x mairo que y ",x>y)  
+print (" x menor que y ",x<y)  
+print ("x menor ou igual que y",x<=y) 
+print ("x maior ou igual que y",x>=y) 
+
+#########################################
+# Operadores aritméticos
+#########################################
+print ("Operadores aritméticos")
+print("------------------------")
+print("soma =",5 + 2)   # Soma → 7
+print("Subtração =",5 - 2)   # Subtração → 3
+print("multiplicação =",2 * 5)   # Multiplicação → 10
+print("divisão =",5 / 2)   # Divisão → 2.5
+print("Divisão inteira =",5 // 2)  # Divisão inteira → 2
+print("resto da divisão =",5 % 2)   # Resto da divisão → 1
+print("potencia =",5 ** 2)  # Potência → 25
+
+
+#########################################
+# Operadores lógicos
+#########################################
+print ("Operadores lógicos")
+print("------------------------")
+p, q = True, False
+
+print("p and q:", p and q)  # AND lógico
+print("p or q:", p or q)    # OR lógico
+print("not p:", not p)      # NOT lógico
+
 
--- op4.py
+++
@@ -1,7 +0,0 @@
-
-# Operadores lógicos
-p, q = True, False
-
-print("p and q:", p and q)  # AND lógico
-print("p or q:", p or q)    # OR lógico
-print("not p:", not p)      # NOT lógico
